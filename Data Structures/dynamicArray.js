8e10797cf5cecd218d43aa48ff840d88b41659dc
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2024-05-02 17:30:08 -0400

    Create dynamic array.

---
+++ Data Structures/dynamicArray.js
@@ -0,0 +1,23 @@
+class DynamicArr {
+    constructor() {
+        this.length = 0;
+        this.maxCapacity = 1;
+        this.array = new Array(this.maxCapacity);
+    }
+
+    addElement() {
+
+    }
+
+    deleteElement() {
+        
+    }
+
+    resize() {
+        this.array = new Array(this.maxCapacity * 2);
+    }
+
+    getIndex(i) {
+
+    }
+}
