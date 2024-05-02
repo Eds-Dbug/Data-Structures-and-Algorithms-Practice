d5f623bf41ed39b07c630ad546cea68a7c68f1b8
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2024-05-02 17:49:21 -0400

    Create core resize method

--- Data Structures/dynamicArray.js
+++ Data Structures/dynamicArray.js
@@ -1,20 +1,29 @@
 class DynamicArr {
     constructor() {
-        this.length = 0;
+        this.currentLength = 0;
         this.maxCapacity = 1;
         this.array = new Array(this.maxCapacity);
     }
 
     addElement() {
+        if(this.currentLength == this.maxCapacity) {
+            this.resize();
+        }
 
     }
 
     deleteElement() {
-        
+
     }
 
     resize() {
-        this.array = new Array(this.maxCapacity * 2);
+        let newArr = new Array(this.maxCapacity * 2);
+        this.maxCapacity *= 2;
+        for(let i=0; i < this.currentLength; i++) {
+            newArr[i] = this.array[i]
+        } 
+        this.array = null;
+        this.array = newArr;
     }
 
     getIndex(i) {
