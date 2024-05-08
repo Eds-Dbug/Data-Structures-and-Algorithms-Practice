409f1771912225515796694e1f12d0a1cabe5c1a
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2024-05-08 17:18:22 -0400

    Do prefix array

--- Data Structures/prefixSumArr.js
+++ Data Structures/prefixSumArr.js
@@ -1,1 +1,15 @@
-let arr = [10,20,30];
+const prefixSums = (arr,n) => {
+    let prefixArr = new Array(n);
+    //initialize the first element
+    prefixArr[0] = arr[0];
+    for(let i=1; i<n; i++){
+        prefixArr[i] = prefixArr[i-1] + arr[i];
+    }
+    return prefixArr;
+}
+
+//driver
+
+let arr = [ 10, 4, 16, 20 ];
+let n = arr.length;
+console.log(prefixSums(arr,n));
