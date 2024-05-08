19d833595615440ff63d2e4cb5661232ca0bdba5
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2024-05-08 19:51:20 -0400

    try just multiplying and moving only when something is bigger

--- NeetCode/Pointers/maxArea.js
+++ NeetCode/Pointers/maxArea.js
@@ -3,7 +3,18 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    
+    let left = 0
+    let right = height.length -1;
+
+    let max = height[left]*height[right];
+
+    while(height[left++] > height[left] || height[right--] > height[right]){
+        max = height[left] * height[right]
+    }
+
+    return max;
 };
 
 //driver code
+console.log(maxArea([1,8,6,2,5,4,8,3,7]));
+console.log(maxArea([1,1]));
