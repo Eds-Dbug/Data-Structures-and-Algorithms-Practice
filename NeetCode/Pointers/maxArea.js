37bd29cb2f9b6617d0d06e908d5ebe3f4ef78cf7
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2024-05-08 21:13:52 -0400

    new max area with left rioght pointer.

--- NeetCode/Pointers/maxArea.js
+++ NeetCode/Pointers/maxArea.js
@@ -3,18 +3,31 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let left = 0
-    let right = height.length -1;
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0; 
 
-    let max = height[left]*height[right];
+    while (left < right) {
+        let currentWidth = right - left;
+        let currentHeight = Math.min(height[left], height[right]);
+        let currentArea = currentHeight * currentWidth;
+        maxArea = Math.max(maxArea, currentArea); 
 
-    while(height[left++] > height[left] || height[right--] > height[right]){
-        max = height[left] * height[right]
+        if (currentArea > maxArea) {  
+            maxArea = currentArea; 
+        }
+
+        if (height[left] < height[right]) {
+            left++;
+        }else {
+            right--;
+        }
     }
 
-    return max;
+    return maxArea;
 };
 
-//driver code
-console.log(maxArea([1,8,6,2,5,4,8,3,7]));
-console.log(maxArea([1,1]));
+// Driver code
+console.log(maxArea([1,8,6,2,5,4,8,3,7])); // Test with provided data
+console.log(maxArea([1,1])); // Test with minimum data
+console.log(maxArea([1,2,1])); // Test with increasing-decreasing pattern
