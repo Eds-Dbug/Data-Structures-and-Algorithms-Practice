0ce25f2fe88eeb30a364cd82b86d5dceb5423b2d
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2024-05-08 21:13:59 -0400

    comment

--- NeetCode/Pointers/maxArea.js
+++ NeetCode/Pointers/maxArea.js
@@ -6,7 +6,7 @@ var maxArea = function(height) {
     let left = 0;
     let right = height.length - 1;
     let maxArea = 0; 
-
+    // so when I will use a default condition whenever i dont have a clear stopping condition.
     while (left < right) {
         let currentWidth = right - left;
         let currentHeight = Math.min(height[left], height[right]);
@@ -28,6 +28,6 @@ var maxArea = function(height) {
 };
 
 // Driver code
-console.log(maxArea([1,8,6,2,5,4,8,3,7])); // Test with provided data
-console.log(maxArea([1,1])); // Test with minimum data
-console.log(maxArea([1,2,1])); // Test with increasing-decreasing pattern
+console.log(maxArea([1,8,6,2,5,4,8,3,7]))
+console.log(maxArea([1,1])); 
+console.log(maxArea([1,2,1])); 
