51e9723beb494b52a36667808e4d44a491824361
Eds-Dbug <edsDbug@gmail.com>
2024-07-17 18:22:42 -0400

    Add test cases

--- NeetCode/Pointers/RemoveDuplicates.js
+++ NeetCode/Pointers/RemoveDuplicates.js
@@ -3,5 +3,10 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    
+    let l = 1;
+
+    for
 };
+
+console.log(`Remove Duplicates ${removeDuplicates([1,1,2])}`);
+console.log(`Remove Duplicates ${removeDuplicates([0,0,1,1,1,2,2,3,3,4])}`);
