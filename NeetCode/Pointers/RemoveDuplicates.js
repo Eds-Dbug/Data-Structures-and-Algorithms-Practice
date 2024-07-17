ec6772abb7a2a7743323f12f18734be54e06905c
Eds-Dbug <edsDbug@gmail.com>
2024-07-17 18:36:18 -0400

    fix index.

--- NeetCode/Pointers/RemoveDuplicates.js
+++ NeetCode/Pointers/RemoveDuplicates.js
@@ -6,9 +6,9 @@ var removeDuplicates = function(nums) {
     //Both L and R pointers start at 1
     let l = 1;
     
-    for(let r = 1; i < nums.length; r++) {
+    for(let r = 1; r < nums.length; r++) {
         //If the right pointer is the same as prev val then set the left as the current value
-        if(nums[r] === nums[r-1]){
+        if(nums[r-1] != nums[r]){
             nums[l] = nums[r];
             // increment the left
             l++;
@@ -18,5 +18,5 @@ var removeDuplicates = function(nums) {
     return l;
 };
 
-console.log(`Remove Duplicates ${removeDuplicates([1,1,2])}`);
-console.log(`Remove Duplicates ${removeDuplicates([0,0,1,1,1,2,2,3,3,4])}`);
+console.log(`${removeDuplicates([1,1,2])} duplicates`);
+console.log(`${removeDuplicates([0,0,1,1,1,2,2,3,3,4])} duplicates`);
