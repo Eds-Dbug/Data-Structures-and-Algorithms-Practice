f3b29cbce5e808937bb9db44688359be17f76f3a
Eds-Dbug <edsDbug@gmail.com>
2024-07-17 18:25:10 -0400

    Initiate pointers

--- NeetCode/Pointers/RemoveDuplicates.js
+++ NeetCode/Pointers/RemoveDuplicates.js
@@ -3,9 +3,10 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+    //Both L and R pointers start at 1
     let l = 1;
-
-    for
+    
+    for(let r = 1; i < nums.length; r++)
 };
 
 console.log(`Remove Duplicates ${removeDuplicates([1,1,2])}`);
